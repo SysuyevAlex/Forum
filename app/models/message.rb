b3fc14cc2c580f323d502d6a8177477000717edb
SysuyevAlex <sysuyevalex@gmail.com>
2010-07-12 14:38:59 +0300

    --all

--- app/models/message.rb
+++
@@ -1,4 +0,0 @@
-class Message < ActiveRecord::Base
-	belongs_to :topic;
-	belongs_to :user;
-end
--- ����� �����/app/models/topic.rb
+++
@@ -1,5 +0,0 @@
-class Topic < ActiveRecord::Base
-	belongs_to :forum
-	has_many :messages, :dependent=> :destroy
-	belongs_to :user
-end
