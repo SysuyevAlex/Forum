7892adb81476af6b6616e88bd7066bd1a6d23ab6
SysuyevAlex <sysuyevalex@gmail.com>
2010-07-21 16:28:40 +0300

    'Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#   
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Major.create(:name => 'Daley', :city => cities.first)
+#User.delete_all
+Forum.delete_all
+Topic.delete_all
+Message.delete_all
+
+admin=User.create({:name=>'Admin', :email=>'probitiy@admin.ru', :password=>'11111', :password_confirmation=>'33333', :role=>1})
+user=User.create({:name=>'FirstUser', :email=>'probitiy@user.ru', :password=>'22222', :password_confirmation=>'44444', :role=>0})
+
+forums = Forum.create([{:name=>'First test forum', :create_time=>Time.now-3.month, :user_id=>admin.id},
+			{:name=>'Forum form Seed', :create_time=>Time.now-2.week, :user_id=>admin.id},
+			{:name=>'Last forum', :create_time=>Time.now, :user_id=>admin.id}])
+topics = Topic.create([{:name=>'First topic', :create_time=>Time.now-1.month, :user_id=>user.id, :last_edit=>Time.now, :forum_id=>Forum.find(:first).id},
+			{:name=>'Second Topic', :create_time=>Time.now-1.week, :user_id=>user.id, :last_edit=>Time.now, :forum_id=>Forum.find(:first).id},
+			{:name=>'Third Topic', :create_time=>Time.now, :user_id=>user.id, :last_edit=>Time.now, :forum_id=>Forum.find(:first).id}])
+messages = Message.create([{:name=>'About first topic',:content=>'Its hard to say something about it', :create_time=>Time.now-1.month, :user_id=>user.id, :topic_id=>Topic.find(:first).id},
+			{:name=>'Answer N2',:content=>'Its my topic and I would happy to create something like it', :create_time=>Time.now-1.month, :user_id=>user.id, :topic_id=>Topic.find(:first).id},
+			{:name=>'Админ негодует',:content=>'Хорош по буржуйскому писать. Всех забаню', :create_time=>Time.now-1.month, :user_id=>admin.id, :topic_id=>Topic.find(:first).id}])
--- spec/models/search_spec.rb
+++ spec/models/search_spec.rb
@@ -1,13 +1,14 @@
-﻿require 'spec_helper'
+require 'spec_helper'
 describe Search do
   before(:each) do
-    @search=new Search;
+    @search=Search.new;
   end
 
-  it "should find one topic add topic" do		
-    @search.get_topics('dd', 0, 1, 'year').find(:first).name.should eql('Проверка add topic') ;
+  it "should find one topic with First" do
+    @search.get_topics('First', 0, 1, 'year').length.should eql(1) ;
+    #@search.get_topics('First', 0, 1, 'year').find(:first).name.should eql('First Topic') ;
   end
-  it "should find three topics" do		
-    @search.get_topics('d', 1, 0, 'year').count.should eql(3) ;
+  it "should find one topic" do		
+    @search.get_topics('o', 1, 0, 'year').size.should eql(1) ;
   end
 end
